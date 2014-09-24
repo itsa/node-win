81ffd8387d303487f4754e7bb930e21ba8ac4e1c
Marco Asbreuk <marco@itsasbreuk.nl>
2014-09-24 13:32:42 +0200

    transport module

---
+++ node-win.js
@@ -0,0 +1,382 @@
+"use strict";
+var xmlhttprequest = require('./lib/XMLHttpRequest.js').XMLHttpRequest,
+	Url = require('url');
+
+var used = {};
+
+var count = function (method) {
+	if (!used[method]) {
+		used[method] = 1;
+	} else {
+		used[method] += 1;
+	}
+};
+var vNodeParser = /(?:(^|#|\.)([^#\.\[\]]+))|(\[.+?\])/g;
+
+
+var window = {},
+	doc = window.document = {};
+
+doc.childNodes = [];
+doc.createElement = function(tag) {
+	count('createElement');
+	return {
+		style: {},
+		childNodes: [],
+		nodeName: tag.toUpperCase(),
+		appendChild: doc.appendChild,
+		removeChild: doc.removeChild,
+		replaceChild: doc.replaceChild,
+		insertBefore: function(node, reference) {
+			count('insertBefore');
+			node.parentNode = this;
+			var referenceIndex = this.childNodes.indexOf(reference);
+			if (referenceIndex < 0) {
+				this.childNodes.push(node);
+			} else {
+				var index = this.childNodes.indexOf(node);
+				this.childNodes.splice(referenceIndex, index < 0 ? 0 : 1, node);
+			}
+		},
+		insertAdjacentHTML: function(position, html) {
+			count('insertAdjacentHTML');
+
+			//todo: accept markup
+			if (position == "beforebegin") {
+				this.parentNode.insertBefore(doc.createTextNode(html), this);
+			} else if (position == "beforeend") {
+				this.appendChild(doc.createTextNode(html));
+			}
+		},
+		setAttribute: function(name, value) {
+			count('setAttribute');
+			this[name] = value.toString();
+			if (name == 'href') {
+				var url = Url.parse(value);
+				this.pathname = url.pathname;
+				if (url.search) this.search = url.search;
+			}
+		},
+		setAttributeNS: function(namespace, name, value) {
+			count('setAttributeNS');
+			this.namespaceURI = namespace;
+			this[name] = value.toString();
+		},
+		getAttribute: function(name, value) {
+			count('getAttribute');
+			return this[name];
+		},
+		addEventListener: doc.addEventListener,
+		removeEventListener: doc.removeEventListener,
+		dispatchEvent: doc.dispatchEvent,
+		// matchesSelector should match nested selectors like: "#someid .somedivclass button.someclass"
+		matchesSelector: function (selector) {
+			var selList = selector.replace(/( )+/g, ' ').split(' '),
+			    size = selList.length,
+			    matches, i, selectorItem, node, selMatch;
+			matches = function (checkNode, sel) {
+				count('matchesSelector');
+				// don't forget to reset the found position of the previous search:
+				vNodeParser.lastIndex = 0;
+				var match,
+					found = false,
+					classes = checkNode.className && checkNode.className.split(' ');
+				/*jshint boss:true*/
+				while (match = vNodeParser.exec(sel)) {
+					/*jshint boss:false*/
+					switch (match[1]) {
+						case "":
+							if (checkNode.nodeName !== match[2].toUpperCase()) return false;
+							found = true;
+							break;
+						case "#":
+							if (checkNode.id !== match[2]) return false;
+							found = true;
+							break;
+						case ".":
+
+							if (!classes || classes.indexOf(match[2]) === -1) return false;
+							found = true;
+							break;
+					}
+				}
+				return found;
+			};
+			if (size===0) {
+				return false;
+			}
+			node = this;
+			selectorItem = selList[size-1];
+            selMatch = matches(node, selectorItem);
+			for (i=size-2; (selMatch && (i>=0)); i--) {
+				selectorItem = selList[i];
+	            node = node.parentNode;
+	            while (node && !(selMatch=matches(node, selectorItem))) {
+	            	node = node.parentNode;
+	            }
+			}
+			return selMatch;
+		},
+		getElementById: doc.getElementById,
+		contains: doc.contains
+	};
+};
+doc.createElementNS = function(namespace, tag) {
+	count('createElementNS');
+	var element = doc.createElement(tag);
+	element.namespaceURI = namespace;
+	return element;
+};
+doc.createTextNode = function(text) {
+	count('createTextNode');
+	return {nodeValue: text.toString()};
+};
+doc.documentElement = doc.createElement("html");
+doc.replaceChild = function(newChild, oldChild) {
+	count('replaceChild');
+	var index = this.childNodes.indexOf(oldChild);
+	if (index > -1) this.childNodes.splice(index, 1, newChild);
+	else this.childNodes.push(newChild);
+	newChild.parentNode = this;
+	oldChild.parentNode = null;
+};
+doc.appendChild = function(child) {
+	count('appendChild');
+	var index = this.childNodes.indexOf(child);
+	if (index > -1) this.childNodes.splice(index, 1);
+	this.childNodes.push(child);
+	child.parentNode = this;
+};
+doc.removeChild = function(child) {
+	count('removeChild');
+	var index = this.childNodes.indexOf(child);
+	this.childNodes.splice(index, 1);
+	child.parentNode = null;
+};
+doc.addEventListener = function (type, cb, capture) {
+	count('addEventListener');
+	if (!this.$on) this.$on = {};
+	this.$on[type] = {
+		cb: cb,
+		capture: capture
+	};
+};
+doc.removeEventListener = function (type) {
+	count('removeEventListener');
+	if (this.$on && this.$on[type]) {
+		delete this.$on[type];
+	}
+};
+var EventTypes = {
+	MouseEvents: function () {
+		this.initMouseEvent = function (type, bubbles, cancelable, view, detail,
+				screenX, screenY, clientX, clientY,
+				ctrlKey, altKey, shiftKey, metaKey,
+				button, relatedTarget) {
+			count('initMouseEvent');
+			this.ev = {
+				type:type,
+				bubbles:bubbles,
+				cancelable:cancelable,
+				view:view,
+				detail:detail,
+				screenX:screenX,
+				screenY:screenY,
+				clientX:clientX,
+				clientY:clientY,
+				ctrlKey:ctrlKey,
+				altKey:altKey,
+				shiftKey:shiftKey,
+				metaKey:metaKey,
+				button:button,
+				relatedTarget:relatedTarget
+			};
+		};
+	}
+};
+doc.createEvent = function (type) {
+	count('createEvent');
+	return new EventTypes[type]();
+};
+doc.dispatchEvent = function (event) {
+	count('dispatchEvent');
+	var branch = [],
+		el = this,
+		type = event.ev.type,
+		cb,
+		ev = event.ev;
+	ev.target = this;
+
+	while(el) {
+		if (el.$on && el.$on[type] && el.$on[type].capture) {
+			branch.push(el);
+			branch.push(el.$on[type].cb);
+		}
+		el = el.parentNode;
+	}
+	while ((cb =  branch.pop())) {
+		ev.currentTarget = branch.pop();
+		cb.call(this, ev);
+	}
+	el = this;
+	while (el) {
+		if (el.$on && el.$on[type] && el.$on[type].capture === false) {
+			ev.currentTarget = el;
+			el.$on[type].cb.call(this, ev);
+		}
+		if (el['on' + type]) {
+			ev.currentTarget = el;
+			el['on' + type].call(this, ev);
+		}
+		el = el.parentNode;
+	}
+};
+
+
+doc.getElementById = function (id) {
+	count('getElementById');
+	var found = null,
+		find = function (el) {
+			if (el.id === id) {
+				found = el;
+				return true;
+			}
+			if (el.childNodes) {
+				return el.childNodes.some(find);
+			}
+		};
+	find(doc);
+	return found;
+};
+doc.contains = function (node) {
+	count('contains');
+	while (node && node !== this) node = node.parentNode;
+	return node === this;
+};
+
+window.performance = new function () {
+	var timestamp = 50;
+	this.$elapse = function(amount) {
+		timestamp += amount;
+	};
+	this.now = function() {
+		return timestamp;
+	};
+};
+window.cancelAnimationFrame = function() {};
+window.requestAnimationFrame = function(callback) {
+	window.requestAnimationFrame.$callback = callback;
+};
+window.requestAnimationFrame.$resolve = function() {
+	if (window.requestAnimationFrame.$callback) window.requestAnimationFrame.$callback();
+	window.requestAnimationFrame.$callback = null;
+	window.performance.$elapse(20);
+};
+window.location = {};
+window.XMLHttpRequest = xmlhttprequest;
+
+var getHTML = function (node) {
+	var prop, val,
+		style, styles = [],
+		html = '';
+
+	if (!node.nodeName && node.nodeValue !== undefined) {
+		// For text nodes, I return the uppercase text
+		// so that you can tell the parts generated at the server
+		// from the normal lowercase of the actual app when run on the client
+		return node.nodeValue.toUpperCase();
+	}
+	html += '<' + node.nodeName;
+	for (prop in node) {
+		val = node[prop];
+
+		// Ignore functions, those will be revived on the client side.
+		if (typeof val == 'function') continue;
+		switch (prop) {
+		case 'nodeName':
+		case 'parentNode':
+		case 'childNodes':
+		case 'pathname':
+		case 'search':
+			continue;
+		case 'checked':
+			if (val == 'false') continue;
+			break;
+		case 'href':
+			val = node.pathname;
+			break;
+		case 'className':
+			prop = 'class';
+			break;
+		case 'style':
+			if (val) {
+				for (style in val) {
+					if (val[style]) {
+						styles.push(style + ': ' + val[style]);
+					}
+				}
+				if (!styles.length) continue;
+				val = styles.join(';');
+			}
+			break;
+		}
+		html += ' ' + prop + '="' + val.replace('"', '\\"') + '"';
+	}
+
+	if (node.childNodes.length) {
+		html += '>' + node.childNodes.reduce(function (prev, node) {
+			return prev + getHTML(node);
+		}, '') + '</' + node.nodeName + '>';
+	} else {
+		// I don't know why Mithril assigns the content of textareas
+		// to its value attribute instead of the innerHTML property.
+		// Since it doesn't have children, the closing tag has to be forced.
+		if (node.nodeName == 'TEXTAREA') {
+			html += '></TEXTAREA>';
+		} else {
+			html += '/>';
+		}
+	}
+	return html;
+};
+var reset = function () {
+	window.location.search = "?/";
+	window.location.pathname = "/";
+	window.location.hash = "";
+	window.history = {};
+	window.history.pushState = function(data, title, url) {
+		window.location.pathname = window.location.search = window.location.hash = url;
+	},
+	window.history.replaceState = function(data, title, url) {
+		window.location.pathname = window.location.search = window.location.hash = url;
+	};
+	var _body = doc.createElement('body');
+	doc.appendChild(_body);
+	doc.body = _body;
+};
+reset();
+
+window.navigator = {
+
+	userAgent: 'fake',
+	stats: {
+		clear: function () {
+			used = {};
+		},
+		get: function () {
+			return used;
+		}
+	},
+	reset: reset,
+	getHTML: function () {
+		return getHTML(doc.body);
+	},
+	navigate: function (url) {
+		var u = Url.parse(url, false, true);
+		window.location.search = u.search || '';
+		window.location.pathname = u.pathname || '';
+		window.location.hash = u.hash || '';
+	}
+};
+
+module.exports = window;
---
+++ tests/node-win.js
@@ -0,0 +1,278 @@
+/*global describe, it */
+
+"use strict";
+
+var expect = require('chai').expect,
+    should = require('chai').should(),
+
+    win = require('../node-win.js'),
+    DOCUMENT = win.document,
+    bodyNode = DOCUMENT.body,
+    divnode1, divnode2, buttonnode;
+
+describe('matchesSelector', function () {
+
+        before(function() {
+            bodyNode.id = 'fakebody';
+
+            divnode1 = DOCUMENT.createElement('div');
+            divnode1.id = 'div1';
+            divnode1.className = 'class1a class1b';
+
+            divnode2 = DOCUMENT.createElement('div');
+            divnode2.id = 'div2';
+            divnode2.className = 'class2a class2b';
+
+            buttonnode = DOCUMENT.createElement('button');
+            buttonnode.id = 'button3';
+            buttonnode.className = 'class3a class3b';
+
+            divnode2.appendChild(buttonnode);
+            divnode1.appendChild(divnode2);
+            DOCUMENT.body.appendChild(divnode1);
+        });
+
+        // Code to execute after the tests inside this describegroup.
+        after(function() {
+            DOCUMENT.body.removeChild(divnode1);
+            delete bodyNode.id;
+        });
+
+
+    it('divnode1 --> "#fakebody .class1a"', function () {
+        divnode1.matchesSelector('#fakebody .class1a').should.be.true;
+    });
+
+    it('divnode1 --> "div"', function () {
+        divnode1.matchesSelector('div').should.be.true;
+    });
+
+    it('bodyNode --> "#fakebody"', function () {
+        bodyNode.matchesSelector('#fakebody').should.be.true;
+    });
+
+    it('bodyNode --> "#div1"', function () {
+        bodyNode.matchesSelector('#div1').should.be.false;
+    });
+
+    it('divnode1 --> "#div1"', function () {
+        divnode1.matchesSelector('#div1').should.be.true;
+    });
+
+    it('divnode1 --> "#div1.class1a"', function () {
+        divnode1.matchesSelector('#div1.class1a').should.be.true;
+    });
+
+    it('divnode1 --> "#div1.noclass"', function () {
+        divnode1.matchesSelector('#div1.noclass').should.be.false;
+    });
+
+    it('divnode1 --> "#div1.class1a.class1b"', function () {
+        divnode1.matchesSelector('#div1.class1a.class1b').should.be.true;
+    });
+
+    it('divnode1 --> "#div1 .class1a"', function () {
+        divnode1.matchesSelector('#div1 .class1a').should.be.false;
+    });
+
+    it('divnode1 --> "#div1 .class2a"', function () {
+        divnode1.matchesSelector('#div1 .class2a').should.be.false;
+    });
+
+    it('divnode1 --> "#div1 #div2"', function () {
+        divnode1.matchesSelector('#div1 #div2').should.be.false;
+    });
+
+    it('divnode2 --> "#div1 div"', function () {
+        divnode2.matchesSelector('#div1 div').should.be.true;
+    });
+
+    it('divnode2 --> "#div1 #div2"', function () {
+        divnode2.matchesSelector('#div1 #div2').should.be.true;
+    });
+
+    it('divnode2 --> "#div1 #div2.class2a"', function () {
+        divnode2.matchesSelector('#div1 #div2.class2a').should.be.true;
+    });
+
+    it('divnode2 --> "#div1 div.class2a"', function () {
+        divnode2.matchesSelector('#div1 div.class2a').should.be.true;
+    });
+
+    it('divnode2 --> "#div1 #div2.noclass"', function () {
+        divnode2.matchesSelector('#div1 #div2.noclass').should.be.false;
+    });
+
+    it('divnode2 --> "#div1 #div2.class2a.class2b"', function () {
+        divnode2.matchesSelector('#div1 #div2.class2a.class2b').should.be.true;
+    });
+
+    it('divnode2 --> "#div1 div.class2a.class2b"', function () {
+        divnode2.matchesSelector('#div1 div.class2a.class2b').should.be.true;
+    });
+
+    it('divnode2 --> "#div1 #div2.class2a.noclass"', function () {
+        divnode2.matchesSelector('#div1 #div2.class2a.noclass').should.be.false;
+    });
+
+    it('divnode2 --> "#div1 #div2 .class2a"', function () {
+        divnode2.matchesSelector('#div1 #div2 .class2a').should.be.false;
+    });
+
+    it('divnode2 --> "#div1 #div2 .class3a"', function () {
+        divnode2.matchesSelector('#div1 #div2 .class3a').should.be.false;
+    });
+
+    it('divnode2 --> "#div1 #div2 #button3"', function () {
+        divnode2.matchesSelector('#div1 #div2 #button3').should.be.false;
+    });
+
+    it('divnode2 --> "#div1#div2"', function () {
+        divnode2.matchesSelector('#div1#div2').should.be.false;
+    });
+
+    it('divnode2 --> "#div1#div2.class2a"', function () {
+        divnode2.matchesSelector('#div1#div2.class2a').should.be.false;
+    });
+
+    it('divnode2 --> "#div1#div2.class2aclass2b"', function () {
+        divnode2.matchesSelector('#div1#div2.class2aclass2b').should.be.false;
+    });
+
+    it('divnode2 --> "#div1#div2 .class2a"', function () {
+        divnode2.matchesSelector('#div1#div2 .class2a').should.be.false;
+    });
+
+    it('divnode2 --> "#div1#div2 .class3a"', function () {
+        divnode2.matchesSelector('#div1#div2 .class3a').should.be.false;
+    });
+
+    it('divnode2 --> "#div1#div2 #button3"', function () {
+        divnode2.matchesSelector('#div1#div2 #button3').should.be.false;
+    });
+
+    it('buttonnode --> "#div1 #button3"', function () {
+        buttonnode.matchesSelector('#div1 #button3').should.be.true;
+    });
+
+    it('buttonnode --> "#div1 #button3.class3a"', function () {
+        buttonnode.matchesSelector('#div1 #button3.class3a').should.be.true;
+    });
+
+    it('buttonnode --> "#div1 #button3.class3a.class3b"', function () {
+        buttonnode.matchesSelector('#div1 #button3.class3a.class3b').should.be.true;
+    });
+
+    it('buttonnode --> "#div1 #button3.class3a .class3b"', function () {
+        buttonnode.matchesSelector('#div1 #button3.class3a .class3b').should.be.false;
+    });
+
+    it('buttonnode --> "#div1 #div2 #button3"', function () {
+        buttonnode.matchesSelector('#div1 #div2 #button3').should.be.true;
+    });
+
+    it('buttonnode --> "#div1 #div2 button"', function () {
+        buttonnode.matchesSelector('#div1 #div2 button').should.be.true;
+    });
+
+    it('buttonnode --> "#div1 #div2 #button3.class3a"', function () {
+        buttonnode.matchesSelector('#div1 #div2 #button3.class3a').should.be.true;
+    });
+
+    it('buttonnode --> "#div1 #div2 #button3.class3a.class3b"', function () {
+        buttonnode.matchesSelector('#div1 #div2 #button3.class3a.class3b').should.be.true;
+    });
+
+    it('buttonnode --> "#div1 #div2 button#button3.class3a.class3b"', function () {
+        buttonnode.matchesSelector('#div1 #div2 button#button3.class3a.class3b').should.be.true;
+    });
+
+    it('buttonnode --> "#div1 #div2 button.class3a.class3b"', function () {
+        buttonnode.matchesSelector('#div1 #div2 button.class3a.class3b').should.be.true;
+    });
+
+    it('buttonnode --> "#div1 #div2 #button3.class3a .class3b"', function () {
+        buttonnode.matchesSelector('#div1 #div2 #button3.class3a .class3b').should.be.false;
+    });
+
+    it('buttonnode --> "#div1 #div2 #button3.class3a .class3b"', function () {
+        buttonnode.matchesSelector('#div1 #div2 #button3.class3a .class3b').should.be.false;
+    });
+
+    it('divnode1 --> ".class1a"', function () {
+        divnode1.matchesSelector('.class1a').should.be.true;
+    });
+
+    it('divnode1 --> ".class1a.class1b"', function () {
+        divnode1.matchesSelector('.class1a.class1b').should.be.true;
+    });
+
+    it('divnode1 --> ".class2a"', function () {
+        divnode1.matchesSelector('.class2a').should.be.false;
+    });
+
+    it('divnode2 --> ".class1a .class2a"', function () {
+        divnode2.matchesSelector('.class1a .class2a').should.be.true;
+    });
+
+    it('divnode2 --> ".class1a .class2a.class2b"', function () {
+        divnode2.matchesSelector('.class1a .class2a.class2b').should.be.true;
+    });
+
+    it('divnode2 --> ".class1a .class2a.noclass"', function () {
+        divnode2.matchesSelector('.class1a .class2a.noclass').should.be.false;
+    });
+
+    it('divnode2 --> ".class1a.class1b .class2a.class2b"', function () {
+        divnode2.matchesSelector('.class1a.class1b .class2a.class2b').should.be.true;
+    });
+
+    it('divnode2 --> ".noclass.class1b .class2a.class2b"', function () {
+        divnode2.matchesSelector('.noclass.class1b .class2a.class2b').should.be.false;
+    });
+
+    it('divnode2 --> ".class1a .class2a .class3a"', function () {
+        divnode2.matchesSelector('.class1a .class2a .class3a').should.be.false;
+    });
+
+    it('divnode2 --> ".class1a .class2a.class2b .class3a"', function () {
+        divnode2.matchesSelector('.class1a .class2a.class2b .class3a').should.be.false;
+    });
+
+    it('divnode2 --> ".class1a.class1b .class2a.class2b .class3a"', function () {
+        divnode2.matchesSelector('.class1a.class1b .class2a.class2b .class3a').should.be.false;
+    });
+
+    it('buttonnode --> ".class1a #div2 .class3a"', function () {
+        buttonnode.matchesSelector('.class1a #div2 .class3a').should.be.true;
+    });
+
+    it('buttonnode --> ".class1a #div2.class2b .class3a"', function () {
+        buttonnode.matchesSelector('.class1a #div2.class2b .class3a').should.be.true;
+    });
+
+    it('buttonnode --> ".class1a #div2.class2a.class2b .class3a"', function () {
+        buttonnode.matchesSelector('.class1a #div2.class2a.class2b .class3a').should.be.true;
+    });
+
+    it('buttonnode --> ".noclass #div2.class2a.class2b .class3a"', function () {
+        buttonnode.matchesSelector('.noclass #div2.class2a.class2b .class3a').should.be.false;
+    });
+
+    it('buttonnode --> ".class1a #div2.noclass.class2b .class3a"', function () {
+        buttonnode.matchesSelector('.class1a #div2.noclass.class2b .class3a').should.be.false;
+    });
+
+    it('buttonnode --> ".class1a #div2.class2a.class2b .noclass"', function () {
+        buttonnode.matchesSelector('.class1a #div2.class2a.class2b .noclass').should.be.false;
+    });
+
+    it('buttonnode --> ".class1a.class1b #div2.class2a .class3a"', function () {
+        buttonnode.matchesSelector('.class1a.class1b #div2.class2a .class3a').should.be.true;
+    });
+
+    it('buttonnode --> ".class1a.class1b #div2.class2a.class2b .class3a"', function () {
+        buttonnode.matchesSelector('.class1a.class1b #div2.class2a.class2b .class3a').should.be.true;
+    });
+
+});
